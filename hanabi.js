f00251e9347b3a27a7f6c74a015964b8d3d9ca51
Wakkey <wakkeyuc@gmail.com>
2015-10-07 08:18:23 +0900

    UpDate
    キャラを動かすコマンドを追加。

--- hanabi.js
+++ hanabi.js
@@ -1,7 +1,5 @@
 
 
-
-
 function up(i,span) {
    i = i +  span;
    return i;
@@ -12,21 +10,168 @@ function down(i,span) {
    return i;
 }
 
-function add_stage(parnt,canvas_height,canvas_width) {
+function add_stage(parnt,content,canvas_name,canvas_height,canvas_width) {
     div_element = document.createElement("div");
-    div_element.innerHTML = '<canvas id="canvas1" width="'+canvas_width+'" height="'+canvas_height+'"></canvas>';
+    div_element.innerHTML = '<canvas id='+canvas_name+' width="'+canvas_width+'" height="'+canvas_height+'"></canvas>';
     parent_object = document.getElementById(parnt);
     parent_object.appendChild(div_element);
 
-    canvas = document.getElementById("canvas1");
-    context = canvas.getContext("2d");
+    canvas = document.getElementById(canvas_name);
 }
 
-function create_pic(img,img_file,top,left,size_height,size_width){
+
+
+function create_pic(img,img_file,top,left,size_height,size_width,angle){
     img.onload = function(){
-      context.drawImage(img,top,left,size_width,size_height);
+      var pnt = canvas.getContext("2d");
+      pnt.clearRect(0,0,canvas.width,canvas.height);
+      pnt.drawImage(img,top,left,size_width,size_height);
+      var angles = angle * Math.PI / 180;
+      pnt.rotate(angles);
+
     };
     img.src = img_file;
 }
+
+function create_text(text,size_font,top,left,angle){
+      var pnt = canvas.getContext("2d");
+      pnt.clearRect(0,0,canvas.width,canvas.height);
+      pnt.font = size_font + 'px "ヒラギノ角ゴ Pro"';
+      //var font_width = context.measureText(message).width;
+      pnt.fillStyle = "black";
+      pnt.textBaseline = "bottom";
+      pnt.fillText(text, top,left);
+      var angles = angle * Math.PI / 180;
+      pnt.rotate(angles);
+}
+
+function move_left(pic,left,top,size_width,size_height,angle,rotate,interval) {
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left++,top,size_width,size_height,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+function pic_move_right(pic,left,top,size_width,size_height,angle,rotate,interval){
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left--,top,size_width,size_height,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+function pic_move_top(pic,left,top,size_width,size_height,angle,rotate,interval){
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left,top--,size_width,size_height,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+function pic_move_down(pic,left,top,size_width,size_height,angle,rotate,interval){
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left,top++,size_width,size_height,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+
+
+function move_left(pic,left,top,size_width,size_height) {
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left++,top,size_width,size_height,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+function pic_move_right(pic,left,top,size_width,size_height){
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left--,top,size_width,size_height,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+function pic_move_top(pic,left,top,size_width,size_height){
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left,top--,size_width,size_height,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+function pic_move_down(pic,left,top,size_width,size_height){
+  function IntervalFunc(){
+    create_pic(image =new Image(),pic,left,top++,size_width,size_height,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+
+
+
+function text_move_left(text,size_font,top,left,angle,rotate,interval) {
+  function IntervalFunc(){
+    create_text(text,size_font,top,left++,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+function text_move_right(text,size_font,top,left,angle,rotate,interval){
+  function IntervalFunc(){
+    create_text(text,size_font,top,left--,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+function text_move_top(text,size_font,top,left,angle,rotate,interval){
+  function IntervalFunc(){
+    create_text(text,size_font,top--,left,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+function text_move_down(text,size_font,top,left,angle,rotate,interval){
+  function IntervalFunc(){
+    create_text(text,size_font,top++,left,up(angle,rotate));
+  }
+  var interval_id = setInterval(IntervalFunc , interval);
+}
+
+
+
+function text_move_left(text,top,left) {
+  function IntervalFunc(){
+    create_text(text,20,top,left++,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+function text_move_right(text,top,left){
+  function IntervalFunc(){
+    create_text(text,20,top,left--,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+function text_move_top(text,top,left){
+  function IntervalFunc(){
+    create_text(text,20,top--,left,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+function text_move_down(text,top,left){
+  function IntervalFunc(){
+    create_text(text,20,top++,left,0);
+  }
+  var interval_id = setInterval(IntervalFunc , 100);
+}
+
+
+
+
+
+
+
+
+
+
 
 
